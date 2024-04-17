1ba6ae524f779b9efc802754eaf9234afc8e62d1
muehlke <jens.fernandez.muehlke@mni.thm.de>
2024-04-17 13:23:07 +0200

    feat(todo-comments): add keymap for showing ToDos

--- lua/user/plugins/todo-comments.lua
+++ lua/user/plugins/todo-comments.lua
@@ -6,6 +6,10 @@ local TodoComments = { "folke/todo-comments.nvim" }
 
 TodoComments.dependencies = { "nvim-lua/plenary.nvim" }
 
+TodoComments.init = function()
+    vim.keymap.set("n", "<leader>tt", "<cmd>TodoTelescope<CR>", { desc = "Show ToDos" })
+end
+
 TodoComments.config = function(_, opts)
     require("todo-comments").setup(opts)
 end
