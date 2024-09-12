0184fa7def8c5ccad452672fc5a79c87538955ac
Jens Fernández Mühlke <jens.fernandez.muehlke@mni.thm.de>
2024-09-12 16:48:40 +0200

    feat(conform): install luaformatter over Mason and adapt config
    To be able to download LuaFormatter the Lua header files should be
    installed before with sudo apt install liblua5.3-dev.

--- lua/user/plugins/conform.lua
+++ lua/user/plugins/conform.lua
@@ -7,24 +7,23 @@ local Conform = { "stevearc/conform.nvim" }
 Conform.event = { "BufReadPre", "BufNewFile" }
 
 Conform.config = function(_, opts)
-    opts.formatter_by_ft = {
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        liquid = { "prettier" },
-        lua = { "lua-format" },
-        python = { "isort", "black" },
-    }
-    require("conform").setup(opts)
+	opts.formatters_by_ft = {
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		lua = { "lua-format" },
+		python = { "isort", "black" },
+	}
+	require("conform").setup(opts)
 
-    -- keymap to format/[m]ake [p]retty, auto-save is disabled
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-        require("conform").format({
-            lsp_fallback = false,
-            async = false,
-            timeout_ms = 1000,
-        })
-    end, { desc = "[m]ake [p]retty (format file or range)" })
+	-- keymap to format/[m]ake [p]retty, auto-save is disabled
+	vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		require("conform").format({
+			lsp_fallback = false,
+			async = false,
+			timeout_ms = 1000,
+		})
+	end, { desc = "[m]ake [p]retty (format file or range)" })
 end
 
 return Conform
